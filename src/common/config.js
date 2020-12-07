e4f727589a21ca4e0a88f4d87f194efbceb7cb12
AKHIL VARDHAN <akhilvar23@gmail.com>
2020-12-07 23:07:06 +0530

    master

---
+++ src/common/config.js
@@ -0,0 +1,15 @@
+var Config =
+{
+    "login": {
+        "username": "admin",
+        "password": "admin"
+    },
+    "auth": {
+        "access-token": "IGQVJVazNtNEVsX3ctbXpGaS1JbVMzYk91VlVCUFpXODltQU0zOGpFR0hyOW4xeThkcHI2aFE1REVOaTJBTVdTbWEtb0RSU0ZArZA0hqdWplX3Q2eFJDLVl4SmhvenRFQmNTdGptLTNiZAUV1NFlaMzI2ZAwZDZD"        
+    },
+    "api": {
+        mediaUrl: "https://graph.instagram.com/me/media?fields=id,caption",
+        userUrl: "https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption"
+    }
+};
+export default Config;
